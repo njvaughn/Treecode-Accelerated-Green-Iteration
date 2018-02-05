a1bff7c05c3fe573a1fc591975f2905dfccea8d3
Nathan Vaughn <njvaughn@umich.edu>
2018-02-05 18:36:44 -0500

    Began AMR testing

--- cell-AMR/methods/cell_data_structure.py
+++ cell-AMR/methods/cell_data_structure.py
@@ -1,9 +1,23 @@
+import numpy as np
+
+
+
 class cell(object):
+        
+    def __init__(self, x,y,z, psi):
+        self.psi = psi
+        self.x = x
+        self.y = y
+        self.z = z
+        self.dx = self.x[1]-self.x[0]
+        self.dy = self.y[1]-self.y[0]
+        self.dz = self.z[1]-self.z[0]
+
+    
+    def gradient_psi(self):
+        self.grad = np.gradient(self.psi,self.dx,self.dy,self.dz,edge_order=2)
     
     
-    def __init__(self, gridpoints, psi_grid):
-        self.gridpoints = gridpoints
-        self.psi_grid = psi_grid
         
     
     
