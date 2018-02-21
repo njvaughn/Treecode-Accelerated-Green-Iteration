09507c03141e99f0cddd16e56f280dae2c31910e
Nathan Vaughn <njvaughn@umich.edu>
2018-02-21 17:41:10 -0500

    Moved recursive walk and the recusrive divide to tree class

--- gridpoint-AMR/src/dataStructs.py
+++ gridpoint-AMR/src/dataStructs.py
@@ -1,6 +1,7 @@
 import numpy as np
+from scipy.interpolate import RegularGridInterpolator
 import itertools
-
+from hydrogenPotential import potential, trueWavefunction
 
                     
 
@@ -25,17 +26,49 @@ class Tree(object):
             gridpoints[i,j,k] = GridPoint(xvec[i],yvec[j],zvec[k])
         
         # generate root cell from the gridpoint objects  
-        self.cells = np.empty((1,1,1),dtype=object)
-        self.cells[0,0,0] = Cell( gridpoints )
-        self.cells[0,0,0].level = 0
+#         self.cells = np.empty((1,1,1),dtype=object)
+#         self.cells[0,0,0] = Cell( gridpoints )
+        self.root = Cell( gridpoints )
+        self.root.level = 0
         print("Root of tree constructed.")
             
-    def buildTree(self):
-        # call the recursive divison on the root of the tree
-        self.maxDepth = self.cells[0,0,0].recursiveDivide(maxDepth=0,currentLevel=0)
+    def buildTree(self,minLevels,maxLevels,divideTolerance): # call the recursive divison on the root of the tree
+        # max depth returns the maximum depth of the tree.  maxLevels is the limit on how large the tree is allowed to be,
+        # regardless of division criteria
+        def recursiveDivide_fromTree(Cell, minLevels, maxLevels, divideTolerance, maxDepthAchieved=0, currentLevel=0):
+            if currentLevel < maxLevels:
+                
+                if currentLevel < minLevels:
+                    Cell.divideFlag = True 
+                else:                             
+                    Cell.checkIfCellShouldDivide(divideTolerance)
+                    
+                if Cell.divideFlag == True:   
+                    Cell.divide()
+                    for i,j,k in itertools.product(range(2),range(2),range(2)):
+                        maxDepthAchieved = recursiveDivide_fromTree(Cell.children[i,j,k], minLevels, maxLevels, divideTolerance, maxDepthAchieved, currentLevel+1)
+                    
+            maxDepthAchieved = max(maxDepthAchieved, currentLevel)                                                                                                                                                       
+            return maxDepthAchieved
         
-    def walkTree(self):
-        self.cells[0,0,0].recursiveWalkMidpoint()
+        self.maxDepthAchieved = recursiveDivide_fromTree(self.root, minLevels, maxLevels, divideTolerance, maxDepthAchieved=0, currentLevel=0 )
+        
+    def walkTree(self, attribute=''):  # walk through the tree, printing out specified data (in this case, the midpoints)
+        def recursiveWalkMidpoint(Cell, attribute):  
+            midpoint = Cell.gridpoints[1,1,1]
+            if hasattr(Cell,attribute):
+                print('Level: ', Cell.level,', midpoint: (', midpoint.x,', ',midpoint.y,', ',midpoint.z,'), ', attribute,': ', getattr(Cell,attribute))
+            elif hasattr(midpoint,attribute):
+                print('Level: ', Cell.level,', midpoint: (', midpoint.x,', ',midpoint.y,', ',midpoint.z,'), ', attribute,': ', getattr(midpoint,attribute))
+            else:
+                print('Level: ', Cell.level,', midpoint: (', midpoint.x,', ',midpoint.y,', ',midpoint.z,')')
+            if hasattr(Cell,'children'):
+                print()
+                for i,j,k in itertools.product(range(2),range(2),range(2)):
+                    recursiveWalkMidpoint(Cell.children[i,j,k],attribute)
+                print()
+                
+        recursiveWalkMidpoint(self.root, attribute)
         
 
 
@@ -48,52 +81,82 @@ class Cell(object):
         Cell Constructor.  Cell composed of gridpoint objects
         '''
         self.gridpoints = gridpoints
+        self.getCellBoundsAndVolume()
         
-    def getCellBounds(self):
+    def getCellBoundsAndVolume(self):
         self.xmin = self.gridpoints[0,0,0].x
         self.xmax = self.gridpoints[2,2,2].x
         self.ymin = self.gridpoints[0,0,0].y
         self.ymax = self.gridpoints[2,2,2].y
         self.zmin = self.gridpoints[0,0,0].z
         self.zmax = self.gridpoints[2,2,2].z
+        self.volume = (self.xmax-self.xmin)*(self.ymax-self.ymin)*(self.zmax-self.zmin)
         
+    def interpolate_for_division(self):
+        if hasattr(self, 'psi'):
+            psiCoarse = np.empty((3,3,3))
+            xvec = np.array(self.gridpoints[0,0,0].x,self.gridpoints[1,0,0].x,self.gridpoints[2,0,0].x)
+            yvec = np.array(self.gridpoints[0,0,0].y,self.gridpoints[0,1,0].y,self.gridpoints[0,2,0].y)
+            zvec = np.array(self.gridpoints[0,0,0].z,self.gridpoints[0,0,1].z,self.gridpoints[0,0,2].z)
+            for i,j,k in itertools.product(range(3),range(3),range(3)):
+                psiCoarse[i,j,k] = self.gridpoints[i,j,k].psi
+        
+            self.interpolator = RegularGridInterpolator((xvec, yvec, zvec), psiCoarse) 
+        else:
+            print("Can't generate interpolator because psi hasn't been set yet.")
+    
+    def checkIfCellShouldDivide(self, divideTolerance):
+        # perform midpoint method for integral(testFunction) for parent cell
+        # If parent integral differs from the sum of children integrals over the test function
+        # then set divideFlag to true.
+        self.gridpoints[1,1,1].setTestFunctionValue()
+        parentIntegral = self.gridpoints[1,1,1].testFunctionValue*self.volume
+        
+        childrenIntegral = 0.0
+        xmids = np.array([(3*self.xmin+self.xmax)/4, (self.xmin+3*self.xmax)/4])
+        ymids = np.array([(3*self.ymin+self.ymax)/4, (self.ymin+3*self.ymax)/4])
+        zmids = np.array([(3*self.zmin+self.zmax)/4, (self.zmin+3*self.zmax)/4])
+        for i,j,k in itertools.product(range(2),range(2),range(2)):
+            tempChild = GridPoint(xmids[i],ymids[j],zmids[k])
+            tempChild.setTestFunctionValue()
+            childrenIntegral += tempChild.testFunctionValue*(self.volume/8)
+        
+        if abs(parentIntegral-childrenIntegral) > divideTolerance:
+            self.divideFlag = True
+        else:
+            self.divideFlag = False
         
     def divide(self):
         children = np.empty((2,2,2), dtype=object)
-        self.getCellBounds()
         x = np.linspace(self.xmin,self.xmax,5)
         y = np.linspace(self.ymin,self.ymax,5)
         z = np.linspace(self.zmin,self.zmax,5)
         gridpoints = np.empty((5,5,5),dtype=object)
         gridpoints[::2,::2,::2] = self.gridpoints  # the 5x5x5 array of gridpoints should have the original 3x3x3 objects within
+        
+        """ 
+        Will need interpolator when refining and have a psi estimate on the coarse mesh.
+        Don't really need it yet.
+        """
+#         xm,ym,zm = np.meshgrid(x,y,z, indexing='ij')
+#         self.interpolate_for_division() # generate the local interpolator
+#         psi_fine = self.interpolator((xm,ym,zm)) # interpolate psi onto the fine mesh
+        
         for i, j, k in itertools.product(range(5),range(5),range(5)):
             if gridpoints[i,j,k] == None:
                 gridpoints[i,j,k] = GridPoint(x[i],y[j],z[k])
                 
         for i, j, k in itertools.product(range(2),range(2),range(2)):
             children[i,j,k] = Cell(gridpoints[2*i:2*i+3, 2*j:2*j+3, 2*k:2*k+3])
+            children[i,j,k].parent = self # children should point to their parent
             if hasattr(self,'level'):
                 children[i,j,k].level = self.level+1
         
         self.children = children
 
-    def recursiveDivide(self, currentLevel, maxDepth):
-
-        self.getCellBounds()
-        if self.xmax - self.xmin > 0.256:
-            self.divide()
-            for i,j,k in itertools.product(range(2),range(2),range(2)):
-                maxDepth = self.children[i,j,k].recursiveDivide(currentLevel+1, maxDepth)
-            
-        maxDepth = max(maxDepth, currentLevel)
-                
-        return maxDepth
     
-    def recursiveWalkMidpoint(self):
-        print('Level: ', self.level,', midpoint: (', self.gridpoints[1,1,1].x,', ',self.gridpoints[1,1,1].y,', ',self.gridpoints[1,1,1].z,')')
-        if hasattr(self,'children'):
-            for i,j,k in itertools.product(range(2),range(2),range(2)):
-                self.children[i,j,k].recursiveWalkMidpoint()
+            
+
 
         
 class GridPoint(object):
@@ -108,13 +171,28 @@ class GridPoint(object):
         self.x = x
         self.y = y
         self.z = z
+        self.setAnalyticPsi()  # for now, set the analytic psi value.  Eventually, need to use interpolator
     
     def setPsi(self, psi):
         self.psi = psi
+    
+    def setAnalyticPsi(self):
+        self.psi = trueWavefunction(1, self.x,self.y,self.z)
         
-class Mesh(object):
+    def setTestFunctionValue(self):
+        '''
+        Set the test function value.  For now, this can be the single atom single electron wavefunction.
+        Generally, this should be some representative function that we can use apriori to set up the 
+        refined mesh.  Bikash uses single atom densities, or a sum of single atom densities to give an 
+        indication of where he should refine before actually computing the many-atom electron density.
+        '''
+        self.testFunctionValue = trueWavefunction(1, self.x,self.y,self.z)  
+        
+class Mesh(object):  
     '''
-    Mesh object.  Will be constructed out of many cells, which will be constructed out of gridpoints
+    Mesh object.  Will be constructed out of many cells, which will be constructed out of gridpoints.
+    This will be used if we start with pre-existing mesh and want to set up the cell structure.
+    Ideally, we start with a blank slate and can build the tree straight away, never using this mesh object.
     '''
     def __init__(self, xmin,xmax,nx,ymin,ymax,ny,zmin,zmax,nz):
         '''
--- gridpoint-AMR/tests/testDataStructs.py
+++ gridpoint-AMR/tests/testDataStructs.py
@@ -1,23 +1,23 @@
 import unittest
 import numpy as np
 
-from dataStructs import Mesh, Tree
+from dataStructs import Mesh, Tree, Cell, GridPoint
+from hydrogenPotential import potential, trueWavefunction
 
 class TestDataStructures(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
         '''
-        Generate a mesh on [0,1]x[0,1]x[0,1] containing 8 total cells for testing.  
+        Generate a mesh on [-1,1]x[-1,1]x[-1,1] containing 8 total cells for testing.  
         setUp() gets called before every test below.
         '''
-        self.xmin = self.ymin = self.zmin = 0
-        self.xmax = self.ymax = self.zmax = 1
+        self.xmin = self.ymin = self.zmin = -5
+        self.xmax = self.ymax = self.zmax = 5
         self.nx = self.ny = self.nz = 2
         self.mesh = Mesh(self.xmin,self.xmax,self.nx,self.ymin,self.ymax,self.ny,self.zmin,self.zmax,self.nz)
         self.tree = Tree(self.xmin,self.xmax,self.ymin,self.ymax,self.zmin,self.zmax)
-        self.tree.buildTree()
-        
+        self.tree.buildTree( minLevels=0, maxLevels=3, divideTolerance=0.1)
 
     def testNeighborsPointToSameObject(self):
         '''
@@ -44,9 +44,13 @@ class TestDataStructures(unittest.TestCase):
                          "Failed Test:  Psi set from neighboring cell not noticed")
         
     def testCellDivide(self):
+        '''
+        Test 3:  Test cell division.  The original 3x3x3 GridPoint objects should be pointed to by the children
+        in addition to the new GridPoint objects created at the refined level.  Check that the gridpoint data 
+        gets mapped properly (children are in fact the 8 octants).
+        '''
         self.mesh.cells[0,0,0].divide()
         parent = self.mesh.cells[0,0,0]
-        
         # check that previously existing object are now also owned by the children
         self.assertEqual(parent.gridpoints[2,2,2], parent.children[1,1,1].gridpoints[2,2,2],
                           "corner point not mapped to expected child")
@@ -64,20 +68,39 @@ class TestDataStructures(unittest.TestCase):
         self.assertEqual(parent.children[0,0,0].gridpoints[1,1,2], parent.children[0,0,1].gridpoints[1,1,0], 
                          "Neighboring children aren't pointing to same gridpoint object on their shared face")
         
-    def testTreeRootConstruction(self):
-        self.assertEqual(np.shape(self.tree.cells), (1,1,1), "Error forming root of tree")
-        self.assertEqual(self.tree.cells[0,0,0].gridpoints[0,0,0].x, self.xmin, "Error forming root of tree")
-    
+    def testObjectTypesAfterTreeConstruction(self):
+        self.assertIsInstance(self.tree, Tree, "self is not a Tree object.")
+        self.assertIsInstance(self.tree.root, Cell, "Root of tree is not a cell object.")
+        self.assertIsInstance(self.tree.root.gridpoints[0,0,2], GridPoint, "Gridpoints of root are not GridPoint objects.")
+        self.assertIsInstance(self.tree.root.children[0,1,0], Cell, "Children of root are not cell objects.")
+        self.assertIsInstance(self.tree.root.children[0,1,0].gridpoints[2,1,2], GridPoint, "Children's gridpoints are not GridPoint objects.")
+        self.assertIsInstance(self.tree.root.children[0,1,0].gridpoints[2,1,2].x, np.float, "Gridpoint data not numpy floats.")
+        
     def testTreeBuild(self):
+        self.assertEqual(self.tree.root.level, 0, "Root level wasn't equal to 0.")
+        self.assertEqual(self.tree.root.gridpoints[0,0,0].x, self.xmin, "Root's 000 corner point doesn't have correct x value")
+        self.assertEqual(self.tree.root.children[1,0,1].level, 1, "Root's children level wasn't equal to 1.")
+#         self.assertEqual(self.tree.maxDepth, 3, "depth wasn't 2 like expected.  This test depends on domain size and division rule.  Not very general.")
 
+        
+    def testTreePointers(self):
+        # identify 3 generations, doesn't matter which child in each generation
+        grandparent = self.tree.root
+        parent = grandparent.children[0,1,1]
+        child = parent.children[1,0,1]
+        
+        self.assertEqual(np.shape(parent.children), (2,2,2), "Shape of array of children pointers not (2,2,2)")
+        self.assertEqual(parent.parent, grandparent, "Parent's parent isn't the grandparent")
+        self.assertEqual(child.parent, parent, "Child's parent isn't the parent")
+    
+ 
+    def testAnalyticPsiSetting(self):
+        self.assertEqual(self.tree.root.gridpoints[1,1,1].psi, trueWavefunction(1, 0, 0, 0), "root midpoint (the origin) doesn't have correct wavefunction.")
 
-        self.assertEqual(self.tree.cells[0,0,0].level, 0, "Root level wasn't equal to 0.")
-        self.assertEqual(self.tree.cells[0,0,0].children[1,0,1].level, 1, "Root's children level wasn't equal to 1.")
-        self.assertEqual(self.tree.maxDepth, 2, "depth wasn't 2 like expected.  This test depends on domain size and division rule.  Not very general.")
-        
-#     def testMidpointWalk(self):
-#         self.tree.walkTree()
-        
-        
+    def testMidpointWalk(self):
+        self.tree.walkTree(attribute='divideFlag')
+        self.tree.walkTree(attribute='psi')
+        self.tree.walkTree()
+    
 if __name__ == "__main__":
     unittest.main()
