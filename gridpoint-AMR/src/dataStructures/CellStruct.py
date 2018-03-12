Normalization before energy computation. Bug present in kinetic

gridpoint-AMR/src/dataStructures/CellStruct.py
@@ -213,11 +213,18 @@ class Cell(object):
 #             self.divideFlag = True
 #         else:
 #             self.divideFlag = False
+
         self.getPsiVariation()
         if self.psiVariation > divideTolerance:
             self.divideFlag = True
         else:
             self.divideFlag = False
+            
+#         self.divideFlag=False
+#         for i,j,k in ThreeByThreeByThree:
+#             if ( (self.gridpoints[i,j,k].x==0) and (self.gridpoints[i,j,k].y==0) and (self.gridpoints[i,j,k].z==0) and (self.dx > divideTolerance) ):
+#                 self.divideFlag = True
+#                 return
      
     def fillInNeighbors(self, gridpoints): 
         '''
@@ -353,9 +360,19 @@ class Cell(object):
             Dzz = np.gradient(gradient[2],self.dz,edge_order=2,axis=2)
             Laplacian = (Dxx + Dyy + Dzz)  # only use the Laplacian at the midpoint, for now at least
             return Laplacian
-    
         Laplacian = computeLaplacian(self)
-        self.KE = -1/2*self.volume*midpoint.psi*Laplacian[1,1,1]   
+        self.KE = -1/2*self.volume*midpoint.psi*Laplacian[1,1,1]
+            
+#         psi = np.empty((3,3,3))
+#         for i,j,k in ThreeByThreeByThree:
+#             psi[i,j,k] = self.gridpoints[i,j,k].psi
+#         gradient = np.gradient(psi, self.dx, self.dy, self.dz, edge_order=2)
+#         grad = gradient[0]+gradient[1]+gradient[2]
+#         gradPsiSquared = -grad*grad
+#         self.KE = -1/2*self.volume*gradPsiSquared[1,1,1]
+         
+        
+   
         
         
         

gridpoint-AMR/tests/testConvolution.py
@@ -22,7 +22,7 @@ class Test(unittest.TestCase):
         self.xmin = self.ymin = self.zmin = -10
         self.xmax = self.ymax = self.zmax = 10
         self.tree = Tree(self.xmin,self.xmax,self.ymin,self.ymax,self.zmin,self.zmax)
-        self.tree.buildTree( minLevels=3, maxLevels=7, divideTolerance=0.2, printTreeProperties=True)
+        self.tree.buildTree( minLevels=3, maxLevels=7, divideTolerance=0.1, printTreeProperties=True)
         for element in self.tree.masterList:
             element[1].gridpoints[1,1,1].setPsi(np.random.rand(1))
         self.tree.E = -0.5
@@ -42,7 +42,7 @@ class Test(unittest.TestCase):
         print('\nInitial wavefunction errors:     %.3e L2,  %.3e max' %(self.tree.L2NormError, self.tree.maxCellError))
 #         self.tree.visualizeMesh('psi')
         
-        for i in range(15):
+        for i in range(10):
 #             print('Performing Convolution using masterList')
             self.tree.GreenFunctionConvolutionList(timeConvolution=True)
 #             print('Convolution took         %.4f seconds. ' %self.tree.ConvolutionTime)
@@ -50,33 +50,7 @@ class Test(unittest.TestCase):
             print('Convolution wavefunction errors: %.3e L2,  %.3e max' %(self.tree.L2NormError, self.tree.maxCellError))
         self.tree.visualizeMesh('psi')
        
-#         print('Performing Convolution using masterList')
-#         self.tree.GreenFunctionConvolutionList(timeConvolution=True)
-#         print('Convolution took         %.4f seconds. ' %self.tree.ConvolutionTime)
-#         self.tree.computeWaveErrors()
-#         print('Second Convolution wavefunction errors: %.3e L2,  %.3e max' %(self.tree.L2NormError, self.tree.maxCellError))
-#         self.tree.visualizeMesh('psi')
-#         
-#         print('Performing Convolution using masterList')
-#         self.tree.GreenFunctionConvolutionList(timeConvolution=True)
-#         print('Convolution took         %.4f seconds. ' %self.tree.ConvolutionTime)
-#         self.tree.computeWaveErrors()
-#         print('Third Convolution wavefunction errors: %.3e L2,  %.3e max' %(self.tree.L2NormError, self.tree.maxCellError))
-#         self.tree.visualizeMesh('psi')
-#         
-#         print('Performing Convolution using masterList')
-#         self.tree.GreenFunctionConvolutionList(timeConvolution=True)
-#         print('Convolution took         %.4f seconds. ' %self.tree.ConvolutionTime)
-#         self.tree.computeWaveErrors()
-#         print('Fourth Convolution wavefunction errors: %.3e L2,  %.3e max' %(self.tree.L2NormError, self.tree.maxCellError))
-#         self.tree.visualizeMesh('psi')
-#         
-#         print('Performing Convolution using masterList')
-#         self.tree.GreenFunctionConvolutionList(timeConvolution=True)
-#         print('Convolution took         %.4f seconds. ' %self.tree.ConvolutionTime)
-#         self.tree.computeWaveErrors()
-#         print('Fifth Convolution wavefunction errors: %.3e L2,  %.3e max' %(self.tree.L2NormError, self.tree.maxCellError))
-#         self.tree.visualizeMesh('psi')
+
 
 
 if __name__ == "__main__":