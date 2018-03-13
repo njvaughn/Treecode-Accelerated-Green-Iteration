Convolution tested for fixed E.  Normalization working

gridpoint-AMR/src/dataStructures/CellStruct.py
@@ -348,7 +348,7 @@ class Cell(object):
         self.PE = self.volume*midpoint.psi*midpoint.psi*potential(midpoint.x,midpoint.y,midpoint.z, epsilon)
 
     def computeKinetic(self):
-        midpoint = self.gridpoints[1,1,1]
+#         midpoint = self.gridpoints[1,1,1]
         def computeLaplacian(Cell):
             # get the psi values on a grid
             psi = np.empty((3,3,3))
@@ -360,16 +360,16 @@ class Cell(object):
             Dzz = np.gradient(gradient[2],self.dz,edge_order=2,axis=2)
             Laplacian = (Dxx + Dyy + Dzz)  # only use the Laplacian at the midpoint, for now at least
             return Laplacian
-        Laplacian = computeLaplacian(self)
-        self.KE = -1/2*self.volume*midpoint.psi*Laplacian[1,1,1]
+#         Laplacian = computeLaplacian(self)
+#         self.KE = -1/2*self.volume*midpoint.psi*Laplacian[1,1,1]
             
-#         psi = np.empty((3,3,3))
-#         for i,j,k in ThreeByThreeByThree:
-#             psi[i,j,k] = self.gridpoints[i,j,k].psi
-#         gradient = np.gradient(psi, self.dx, self.dy, self.dz, edge_order=2)
-#         grad = gradient[0]+gradient[1]+gradient[2]
-#         gradPsiSquared = -grad*grad
-#         self.KE = -1/2*self.volume*gradPsiSquared[1,1,1]
+        psi = np.empty((3,3,3))
+        for i,j,k in ThreeByThreeByThree:
+            psi[i,j,k] = self.gridpoints[i,j,k].psi
+        gradient = np.gradient(psi, self.dx, self.dy, self.dz, edge_order=2)
+        grad = gradient[0]+gradient[1]+gradient[2]
+        gradPsiSquared = -grad*grad
+        self.KE = -1/2*self.volume*gradPsiSquared[1,1,1]
          
         
    

gridpoint-AMR/src/dataStructures/TreeStruct.py
@@ -366,16 +366,33 @@ class Tree(object):
         self.maxCellError = np.max(errors)
         
     def normalizeWavefunction(self):
+        """ Compute integral psi*2 dxdydz """
         A = 0.0
         for element in self.masterList:
             if element[1].leaf == True:
                 A += element[1].gridpoints[1,1,1].psi**2*element[1].volume
-        
+                
+        """ Initialize the normalization flag for each gridpoint """        
         for element in self.masterList:
             if element[1].leaf==True:
-                element[1].gridpoints[1,1,1].psi /= np.sqrt(A)
+                for i,j,k in ThreeByThreeByThree:
+                    element[1].gridpoints[i,j,k].normalized = False
+        
+        """ Rescale wavefunction values, flip the flag """
+        for element in self.masterList:
+            if element[1].leaf==True:
+                for i,j,k in ThreeByThreeByThree:
+                    if element[1].gridpoints[i,j,k].normalized == False:
+                        element[1].gridpoints[i,j,k].psi /= np.sqrt(A)
+                        element[1].gridpoints[i,j,k].normalized = True
+        
+        """  Delete the flag, if desired               
+        for element in self.masterList:
+            if element[1].leaf==True:
+                for i,j,k in ThreeByThreeByThree:
+                    element[1].gridpoints[i,j,k].normalized = None
             
-            
+        """    
             
             
             

gridpoint-AMR/tests/testConvolution.py
@@ -6,9 +6,7 @@ Created on Mar 10, 2018
 import unittest
 import numpy as np
 from TreeStruct import Tree
-from CellStruct import Cell
-from GridpointStruct import GridPoint
-from hydrogenPotential import trueWavefunction
+
 
 
 class Test(unittest.TestCase):
@@ -22,7 +20,7 @@ class Test(unittest.TestCase):
         self.xmin = self.ymin = self.zmin = -10
         self.xmax = self.ymax = self.zmax = 10
         self.tree = Tree(self.xmin,self.xmax,self.ymin,self.ymax,self.zmin,self.zmax)
-        self.tree.buildTree( minLevels=3, maxLevels=7, divideTolerance=0.1, printTreeProperties=True)
+        self.tree.buildTree( minLevels=3, maxLevels=7, divideTolerance=0.04, printTreeProperties=True)
         for element in self.tree.masterList:
             element[1].gridpoints[1,1,1].setPsi(np.random.rand(1))
         self.tree.E = -0.5