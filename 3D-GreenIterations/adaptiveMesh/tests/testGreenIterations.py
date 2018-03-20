a8c78184fcfa56a8585b5386cda1b6f3365fef4b
Nathan Vaughn <njvaughn@umich.edu>
2018-03-20 14:47:17 -0400

    Switched to total energy residual tolerance

--- 3D-GreenIterations/adaptiveMesh/tests/testGreenIterations.py
+++ 3D-GreenIterations/adaptiveMesh/tests/testGreenIterations.py
@@ -26,10 +26,10 @@ class TestGreenIterations(unittest.TestCase):
         '''
         setUp() gets called before every test below.
         '''
-        self.xmin = self.ymin = self.zmin = -10
-        self.xmax = self.ymax = self.zmax = 10
+        self.xmin = self.ymin = self.zmin = -8
+        self.xmax = self.ymax = self.zmax = 8
         self.tree = Tree(self.xmin,self.xmax,self.ymin,self.ymax,self.zmin,self.zmax)
-        self.tree.buildTree( minLevels=3, maxLevels=7, divideTolerance=0.075, printTreeProperties=True)
+        self.tree.buildTree( minLevels=4, maxLevels=9, divideTolerance=0.05, printTreeProperties=True)
         for element in self.tree.masterList:
             
             element[1].gridpoints[1,1,1].setPsi(np.random.rand(1))
@@ -37,14 +37,14 @@ class TestGreenIterations(unittest.TestCase):
 
 
     
-
+    @unittest.skip('Skipped CPU convolution')    
     def testGreenIterations(self):
         self.tree.E = -1.0 # initial guess
           
         for i in range(1):
             print()
             self.tree.GreenFunctionConvolutionList(timeConvolution=True)
-#             print('Convolution took:                %.4f seconds. ' %self.tree.ConvolutionTime)
+            print('Convolution took:                %.4f seconds. ' %self.tree.ConvolutionTime)
             self.tree.computeWaveErrors()
             print('Convolution wavefunction errors: %.3e L2,  %.3e max' %(self.tree.L2NormError, self.tree.maxCellError))
             self.tree.updateEnergy()
@@ -64,9 +64,10 @@ class TestGreenIterations(unittest.TestCase):
         
         GIcounter=1                                     # initialize the counter to counter the number of iterations required for convergence
         residual = 1                                    # initialize the residual to something that fails the convergence tolerance
-        residualTolerance = 0.0001                      # set the residual tolerance
-        Pold = -10.0                                    # set initial values of potential and kinetic energy, these will be overwritten once the iterations begin.
-        Kold = -10.0
+        residualTolerance = 0.000005                      # set the residual tolerance
+        Eold = -10.0
+#         Pold = -10.0                                    # set initial values of potential and kinetic energy, these will be overwritten once the iterations begin.
+#         Kold = -10.0
         while residual > residualTolerance:
             print()
             print('Green Iteration Count ', GIcounter)
@@ -96,13 +97,15 @@ class TestGreenIterations(unittest.TestCase):
                 self.tree.E = -2
             energyUpdateTime = timer() - startEnergyTime
             print('Energy Update took:              %.4f seconds. ' %energyUpdateTime)
-            residual = max( abs(Pold - self.tree.totalPotential),abs(Kold - self.tree.totalKinetic) )  # Compute the residual for determining convergence
+#             residual = max( abs(Pold - self.tree.totalPotential),abs(Kold - self.tree.totalKinetic) )  # Compute the residual for determining convergence
+            residual = abs(Eold - self.tree.E)  # Compute the residual for determining convergence
             print('Energy Residual:                 %.3e' %residual)
-            Pold = self.tree.totalPotential             # Update the old Potential and Kinetic 
-            Kold = self.tree.totalKinetic
+#             Pold = self.tree.totalPotential             # Update the old Potential and Kinetic 
+#             Kold = self.tree.totalKinetic
+            Eold = self.tree.E
             print('Kinetic Energy Error:            %.5f ' %(self.tree.totalKinetic-0.5))
             print('Potential Energy Error:          %.5f ' %(self.tree.totalPotential+1))
-            print('Updated Energy Value:            %.5f Hartree, %.5f error' %(self.tree.E, self.tree.E+0.5))
+            print('Updated Energy Value:            %.5f Hartree, %.6f error' %(self.tree.E, self.tree.E+0.5))
         print('\nConvergence to a tolerance of %f took %i iterations' %(residualTolerance, GIcounter))
              
 
